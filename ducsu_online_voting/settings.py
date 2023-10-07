2fe68de3fc1fb3186c429c78fa417ec06538ff39
Ariful Islam <116522948+Arif-462@users.noreply.github.com>
2023-10-07 13:30:15 +0300

    Update settings.py
    LAST

--- ducsu_online_voting/settings.py
+++ ducsu_online_voting/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-t2(kww3_47!isa@stn$b6k4d3z@kxn$!#ve#_#inro-bszl+sa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.mysite.org', '*']
 
 
 # Application definition
