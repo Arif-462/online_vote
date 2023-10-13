8a1159e99dbebdfb8a9354aee3fd60cb25dc6357
Ariful Islam <116522948+Arif-462@users.noreply.github.com>
2023-10-13 16:02:31 +0300

    Update settings.py

--- ducsu_online_voting/settings.py
+++ ducsu_online_voting/settings.py
@@ -76,19 +76,19 @@ WSGI_APPLICATION = 'ducsu_online_voting.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-...
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-...
+
 # render postgreSQL database(live) 
-import dj_database_url
-DATABASES = {
-    'default': dj_database_url.parse(env('DATABASE_URL'))
-}
+# import dj_database_url
+# DATABASES = {
+#     'default': dj_database_url.parse(env('DATABASE_URL'))
+# }
 
 
 
